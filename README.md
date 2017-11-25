130a1061687cac26feaf19748b9ce100b5b113a4
Pettyfer <pettyferlove@live.cn>
2017-11-25 12:01:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# china-geojson
+
+
+---
+
+## Install
+
+```bash
+npm install china-geojson --save
+```
+
+## Usage
+
+```js
+var ChinaGeojson = require('china-geojson');
+```
+感谢 [Echarts](http://echarts.baidu.com/)。 
---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  '广州': require('./src/geojson/440100.json'),
+  '韶关': require('./src/geojson/440200.json'),
+  '深圳': require('./src/geojson/440300.json'),
+  '珠海': require('./src/geojson/440400.json'),
+  '汕头': require('./src/geojson/440500.json'),
+  '佛山': require('./src/geojson/440600.json'),
+  '江门': require('./src/geojson/440700.json'),
+  '湛江': require('./src/geojson/440800.json'),
+  '茂名': require('./src/geojson/440900.json'),
+  '肇庆': require('./src/geojson/441200.json'),
+  '惠州': require('./src/geojson/441300.json'),
+  '梅州': require('./src/geojson/441400.json'),
+  '汕尾': require('./src/geojson/441500.json'),
+  '河源': require('./src/geojson/441600.json'),
+  '阳江': require('./src/geojson/441700.json'),
+  '清远': require('./src/geojson/441800.json'),
+  '东莞': require('./src/geojson/441900.json'),
+  '中山': require('./src/geojson/442000.json'),
+  '潮州': require('./src/geojson/445100.json'),
+  '揭阳': require('./src/geojson/445200.json'),
+  '云浮': require('./src/geojson/445300.json')
+}
